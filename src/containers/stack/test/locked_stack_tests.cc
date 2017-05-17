e72d2c866e7803e8b07159f340f4f6a6557a384b
Alexander Pisarev <tresrus92@gmail.com>
2017-05-17 15:55:02 +0300

    stack: add single thread tests

--- src/containers/stack/test/locked_stack_tests.cc
+++ src/containers/stack/test/locked_stack_tests.cc
@@ -1,6 +1,31 @@
 #include "containers/stack/stack.h"
 #include "gtest/gtest.h"
 
-TEST(LockStackTest, Dummy) { 
-    ASSERT_TRUE(true);
+TEST(LockStackTest, EmptyAtStart) { 
+    concurrent::containers::Stack<int> stack;
+    ASSERT_TRUE(stack.Empty());
+}
+
+TEST(LockStackTest, PushOne) { 
+    concurrent::containers::Stack<int> stack;
+    stack.Push(1);
+    ASSERT_FALSE(stack.Empty());
+}
+
+TEST(LockStackTest, PushPop) { 
+    concurrent::containers::Stack<int> stack;
+    stack.Push(1);
+    ASSERT_FALSE(stack.Empty());
+
+    auto value = stack.Pop();
+    ASSERT_TRUE(stack.Empty());
+    ASSERT_EQ(*value, 1);
+}
+
+TEST(LockStackTest, PopEmpty) { 
+    concurrent::containers::Stack<int> stack;
+
+    auto value = stack.Pop();
+    ASSERT_TRUE(stack.Empty());
+    ASSERT_EQ(value, nullptr);
 }
