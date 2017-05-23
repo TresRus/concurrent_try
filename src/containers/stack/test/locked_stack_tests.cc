f6b40f7cc8d8ecbf4f3d94775f555b8a8af0282c
Alexander Pisarev <tresrus92@gmail.com>
2017-05-23 17:55:18 +0300

    stack: add parallel test

--- src/containers/stack/test/locked_stack_tests.cc
+++ src/containers/stack/test/locked_stack_tests.cc
@@ -1,3 +1,6 @@
+#include <thread>
+#include <iostream>
+
 #include "containers/stack/stack.h"
 #include "gtest/gtest.h"
 
@@ -28,4 +31,33 @@ TEST(LockStackTest, PopEmpty) {
     auto value = stack.Pop();
     ASSERT_TRUE(stack.Empty());
     ASSERT_EQ(value, nullptr);
+}
+
+void Push(concurrent::containers::Stack<int>& stack, int max_value) {
+    for (int i = 0; i < max_value; ++i)
+        stack.Push(i);
+}
+
+void Pop(concurrent::containers::Stack<int>& stack, int max_value) {
+    for (int i = 0; i < max_value; ++i) {
+        auto value = stack.Pop();
+        ASSERT_NE(value, nullptr);
+    }
+}
+
+TEST(LockStackTest, PushPopParallel) {
+    concurrent::containers::Stack<int> stack;
+    const int max_value = 10000;
+
+    std::thread t1(Push, std::ref(stack), max_value);
+    Push(stack, max_value);
+    t1.join();
+
+    ASSERT_FALSE(stack.Empty());
+
+    std::thread t2(Pop, std::ref(stack), max_value);
+    Pop(stack, max_value);
+    t2.join();
+
+    ASSERT_TRUE(stack.Empty());
 }
